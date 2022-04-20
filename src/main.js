bee58818b4d61a64f55dae6fcb3ac97f2cb229c6
xxxCxx <1290746759@qq.com>
2022-04-20 17:39:48 +0800

    项目优化-页面顶部增加了进度条(nprogress)

--- src/main.js
+++ src/main.js
@@ -5,10 +5,25 @@ import './assets/css/global.css'
 import axios from 'axios'
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 导入 NProgress
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 Vue.use(ElementUi)
 
-axios.defaults.baseURL = '127.0.0.1:8889'
+axios.defaults.baseURL = 'http://175.178.175.236:8889'
+
+// 在 request 拦截器中，展示进度条 NProgress.start()
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  // 在最后必须 return config
+  return config
+})
+// 在 response 拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
 
 Vue.prototype.$http = axios
 
