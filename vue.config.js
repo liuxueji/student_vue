3ad95ea5205dfcaff94e95408c5ff7816312ac88
xxxCxx <1290746759@qq.com>
2022-04-22 18:45:44 +0800

    修改了默认入口文件，改为开发模式与发布模式两个入口

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,41 @@
+// 通过chainWebpack自定义打包
+// 使用 module.exports 向外暴露
 module.exports = {
-  devServer: {
-    proxy: {
-      '/': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
+  // 使用 chainWebpack 生成节点，
+  chainWebpack: config => {
+    config.when(process.env.NODE_ENV === "production", config => {
+      config
+        .entry("app")
+        .clear()
+        .add("./src/main-prod.js");
+    });
+    config.when(process.env.NODE_ENV === "development", config => {
+      config
+        .entry("app")
+        .clear()
+        .add("./src/main-dev.js");
+    });
   }
-}
+};
+
+// 生成项目体积大小报告
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       new BundleAnalyzerPlugin()
+//     ]
+//   }
+// }
+
+// module.exports = {
+//   proxy正向代理方法
+//   devServer: {
+//     proxy: {
+//       '/': {
+//         target: 'http://localhost:3000',
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// }
